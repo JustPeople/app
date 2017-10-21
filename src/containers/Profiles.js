acd05b980f517304ebd20b8dcecf161195f92a27
Axel Dolce <axel.dolce@gmail.com>
2017-10-21 11:19:07 -0300

    responsive grid

--- src/containers/Profiles.js
+++ src/containers/Profiles.js
@@ -2,14 +2,30 @@ import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 
+import { media, mediaAlt } from '../media'
+
 import * as Selectors from '../selectors'
 
 import ProfileListItem from './ProfileListItem'
 
+//var media = mediaAlt
+
 var ProfilesGrid = styled.div`
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
     grid-gap: 0.5rem;
+    grid-template-columns: repeat(5, 1fr);
+    ${media.desktop`
+        grid-template-columns: repeat(3, 1fr);
+        background: red;
+    `}
+    ${media.tablet`
+        grid-template-columns: repeat(2, 1fr);
+        background: yellow;
+    `}
+    ${media.phone`
+        grid-template-columns: repeat(1, 1fr);
+        background: green;
+    `}
 `
 
 var ProfileList = props => {
