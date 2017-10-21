f6dcf37d82408421d5b8296503a64b4e4db97eb7
Axel Dolce <axel.dolce@gmail.com>
2017-10-21 11:20:09 -0300

    filtered selectors

--- src/selectors.js
+++ src/selectors.js
@@ -3,33 +3,52 @@ export function getProfileById(state, profileId) {
     return profile
 }
 export function getFilteredProfiles(state) {
-    var { filters } = state
-
-    var selectedLocations = getSelectedLocations(state)
-    var selectedlocationsAndChildren = selectedLocations.reduce((sum, id) => {
-        return sum.concat(getChildrenLocations(state, id).map(l => l.id))
-    }, selectedLocations)
-
-    var profiles = state.profiles.filter(profile => {
-        var profileLocation = profile.locationId
-        if (!selectedlocationsAndChildren.length) return true
-        return selectedlocationsAndChildren.indexOf(profile.locationId) !== -1
+    var selectedGenders = getSelectedGenders(state)
+    var selectedLocations = getSelectedLocations(state, {
+        includeChildren: true
     })
+
+    var gendersFilter = makeGendersFilter(selectedGenders)
+    var locationsFilter = makeLocationsFilter(selectedLocations)
+
+    var profiles = state.profiles
+        .filter(gendersFilter)
+        .filter(locationsFilter)
     return profiles
 }
 export function locationById(state, id) {
     var { locations } = state.data
     return locations.find(l => l.id === id)
 }
-export function getSelectedLocations(state) {
+export function getSelectedGenders(state) {
+    var { genders = [] } = state.filters
+    return genders
+}
+export function getSelectedLocations(state, options = {}) {
     var { locations = [] } = state.filters
-    return locations
+    if (!options.includeChildren) return locations
+    return locations.reduce((sum, id) => {
+        return sum.concat(getChildrenLocations(state, id).map(l => l.id))
+    }, locations)
 }
 export function getChildrenLocations(state, locationId, recursive) {
     var { locations } = state.data
-    var children = locations.filter(location => location.parentId == locationId)
+    var children = locations.filter(location => location.parentId === locationId)
     if (!recursive) return children
     return children.map(location => {
-        return getChildrenLocations(state, location.id, recursive)
+        location.children = getChildrenLocations(state, location.id, recursive)
+        return location
     })
+}
+function makeGendersFilter(genders) {
+    return profile => {
+        if (!genders.length) return true
+        return genders.indexOf(profile.gender) !== -1
+    }
+}
+function makeLocationsFilter(locations) {
+    return profile => {
+        if (!locations.length) return true
+        return locations.indexOf(profile.locationId) !== -1
+    }
 }
