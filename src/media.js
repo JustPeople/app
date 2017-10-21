6b854a7141ce40330fd2ce67fae3d77f19d21236
Axel Dolce <axel.dolce@gmail.com>
2017-10-21 11:20:21 -0300

    media

---
+++ src/media.js
@@ -0,0 +1,31 @@
+import { css } from 'styled-components'
+
+const sizes = {
+  giant: 1170,
+  desktop: 992,
+  tablet: 768,
+  phone: 376
+}
+
+// iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+  const emSize = sizes[label]
+  accumulator[label] = (...args) => css`
+    @media (max-width: ${emSize}px) {
+      ${css(...args)}
+    }
+  `
+  return accumulator
+}, {})
+
+export const mediaAlt = Object.keys(sizes).reduce((accumulator, label) => {
+  const pixels = sizes[label]
+  accumulator[label] = (...args) => css`
+    @media (min-width: ${pixels}px) {
+      ${css(...args)}
+    }
+  `
+  return accumulator
+}, {})
