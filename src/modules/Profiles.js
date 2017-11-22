24526665b74e015b55158cccb141659ae5645932
Axel Dolce <axel.dolce@gmail.com>
2017-11-21 21:55:46 -0300

    add method

--- src/modules/Profiles.js
+++ src/modules/Profiles.js
@@ -1,7 +1,27 @@
-var defaultState = require('../api/profiles')
-export default function reducer(state = defaultState, action) {
-    switch (action.type) {
+const ADD = 'ADD_PROFILE'
+
+var initialState = []
+export default function reducer(state = initialState, action) {
+    var { type, payload } = action
+    switch (type) {
+        case ADD: {
+            var updated = false
+            var newState = state.map(profile => {
+                if (profile.id !== payload.id) return profile
+                updated = true
+                return payload
+            })
+            if (!updated) newState.push(payload)
+            return newState
+        }
         default:
             return state
+    }
+}
+
+export function addProfile(profile) {
+    return {
+        type: ADD,
+        payload: profile
     }
 }
