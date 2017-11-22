9471abeafb749c5e224f13f016ed05c923e00176
Axel Dolce <axel.dolce@gmail.com>
2017-11-22 19:11:26 -0300

    re styled

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import Filters from '../containers/ProfileListFilters'
 import ProfilesGrid from '../containers/Profiles'
 
 export default props => {
     return (
-        <div>
-            <Filters />
+        <div style={{ padding: '1rem' }}>
             <ProfilesGrid />
         </div>
     )
