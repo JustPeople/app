a9c5af6c3564482fd706de98d60b8728d8b713ea
Axel Dolce <axel.dolce@gmail.com>
2017-11-21 22:05:28 -0300

    fix

--- src/containers/LocationName.js
+++ src/containers/LocationName.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux'
 import * as S from '../selectors'
 
 export default connect((state, ownProps) => {
-    var location = S.locationById(state, ownProps.id)
+    var location = S.locationById(state, ownProps.id) || {}
     return {
-        name: location.name
+        name: location.name || 'no name for location'
     }
 })(props => <span>{props.name}</span>)
